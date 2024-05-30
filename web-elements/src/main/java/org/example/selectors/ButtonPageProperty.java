866c12a8ae6c5567f799069b1cafa9c57d8bedac
mdzih <dzigmihail@gmail.com>
2024-05-30 21:08:48 +0300

    DQA-007 Add Button Page

---
+++ web-elements/src/main/java/org/example/selectors/ButtonPageProperty.java
@@ -0,0 +1,12 @@
+package org.example.selectors;
+
+public interface ButtonPageProperty {
+    String PATH = "/buttons";
+
+    String DOUBLE_CLICK_BUTTON = "doubleClickBtn";
+    String RIGHT_CLICK_BUTTON = "rightClickBtn";
+    String CLICK_ME_BUTTON = "dR1V1";
+    String DOUBLE_CLICK_MESSAGE = "doubleClickMessage";
+    String RIGHT_CLICK_MESSAGE= "rightClickMessage";
+    String CLICK_ME_MESSAGE = "dynamicClickMessage";
+}
